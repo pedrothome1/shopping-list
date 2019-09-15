4824f2d6780042e4f54b9d3b474cb2204d3a4aec
pedrothome1 <pedrothome2@outlook.com>
2019-09-15 16:24:54 -0300

    customizing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ShoppingList from '../shopping-list';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556080',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#D8625E',
+      contrastText: '#ffffff',
+    },
+  },
+});
+
 export default function app() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <ShoppingList />
-    </>
+    </ThemeProvider>
   );
 }
